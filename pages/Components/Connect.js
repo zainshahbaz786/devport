b2b92db011775e9accbdf4586cb3025a434ed5b3
Zain Shahbaz <zainjatt.zj@gmail.com>
2023-02-25 04:25:10 +0500

    Minor Betterment after long time...

--- pages/Components/Connect.js
+++ pages/Components/Connect.js
@@ -97,7 +97,7 @@ const name = document.querySelector('#username').value
 
 <textarea id="message" rows="4" className="block p-2.5 w-full text-sm
 
-text-gray-900  rounded-lg border border-green-500 focus:ring-green-700 focus:border-green-500 dark: bg-green-100 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
+text-gray-900  rounded-lg border border-green-500 focus:ring-green-700 focus:border-green-500 dark: bg-green-100 dark:border-gray-600 dark:placeholder-black dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
 
 
 
--- pages/Components/SecondPage.js
+++ pages/Components/SecondPage.js
@@ -12,13 +12,13 @@ const SecondPage = () => {
     Things I Love
     </div>
     {/* main div */}
-        <div  className='bg-layers flex p-1/5 justify-around' > 
+        <div  className='bg-layers flex p-1/5 justify-around w4/5' > 
 
 {/* first div */}
                 
 
 
-<div className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+<div className="block p-5 w-1/3 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Front End</h5>
     <p className="font-normal text-gray-700 dark:text-gray-400"> I&apos;m more front end focused and love to work with React JS as well as pure HTML, CSS.
                                         Also pretty good in Angular components as well </p>
@@ -29,7 +29,7 @@ const SecondPage = () => {
 {/* second div */}
                     
 
-                    <div className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <div className="block p-5 w-1/3 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">JavaScript</h5>
     <p className="font-normal text-gray-700 dark:text-gray-400"> I just extremely love javascript, I cant even express how much I love javascript with just a few lines.
                                   </p>
@@ -40,7 +40,7 @@ const SecondPage = () => {
 
                     
 
-                    <div className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <div className="block p-5 w-1/3 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Creative Coding</h5>
     <p className="font-normal text-gray-700 dark:text-gray-400">  I love creative coding because I do both coding & designing. making beautiful art with code is very satisfying to me.
                                   </p>
